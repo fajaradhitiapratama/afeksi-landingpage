324b6d2ff6ab8aa541cad17a91c81e6dbaafeb60
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-03-25 16:16:00 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 const halUrl = window.location.href;
